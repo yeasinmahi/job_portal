4024633fcb4aa06dce29151a4063b39afbe571e0
yeasinmahi <yeasinmahi72@yahoo.com>
2018-02-11 18:23:04 +0600

    added enum

---
+++ DAL/Controller/StartupData.cs
@@ -0,0 +1,56 @@
+﻿using DAL.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Others.Enum;
+
+namespace DAL.Controller
+{
+    class StartupData
+    {
+        readonly ApplicationDbContext _context = ApplicationDbContext.Create();
+        public void AddRole()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+            if (!roleManager.RoleExists(Enums.ToFriendlyString(Enums.DefaultRole.SystemAdmin)))
+            {
+                roleManager.Create(new IdentityRole(Enums.ToFriendlyString(Enums.DefaultRole.SystemAdmin)));
+                roleManager.Create(new IdentityRole(Enums.ToFriendlyString(Enums.DefaultRole.SuperAdmin)));
+                roleManager.Create(new IdentityRole(Enums.ToFriendlyString(Enums.DefaultRole.Admin)));
+            }
+        }
+
+        public void AddUser()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+            ApplicationUser applicationUser = new ApplicationUser
+            {
+                Email = "yeasinmahi72@gmail.com",
+                UserName = "futuristic",
+                EmailConfirmed = true,
+                PhoneNumber = "008801676272718",
+                CustomUserId = 1,
+                PhoneNumberConfirmed = true
+            };
+            IdentityResult result = userManager.Create(applicationUser, "FTL@jp");
+            if (result.Succeeded)
+            {
+                userManager.AddToRole(applicationUser.Id, Enums.ToFriendlyString(Enums.DefaultRole.SystemAdmin));
+            }
+            ApplicationUser superAdmin = new ApplicationUser
+            {
+                Email = "yeasinmahi72@gmail.com",
+                UserName = "superadmin",
+                EmailConfirmed = true,
+                PhoneNumber = "008801676272718",
+                CustomUserId = 1,
+                PhoneNumberConfirmed = true
+            };
+            result = userManager.Create(superAdmin, "FTL@jp");
+            if (result.Succeeded)
+            {
+                userManager.AddToRole(superAdmin.Id, Enums.ToFriendlyString(Enums.DefaultRole.SuperAdmin));
+            }
+        }
+
+    }
+}
---
+++ Others/Enum/Enums.cs
@@ -0,0 +1,31 @@
+﻿namespace Others.Enum
+{
+    public class Enums
+    {
+        public enum DefaultRole
+        {
+            SystemAdmin,
+            SuperAdmin,
+            Admin
+        }
+        public static string ToFriendlyString(DefaultRole defaultRole)
+        {
+            switch (defaultRole)
+            {
+                case DefaultRole.SystemAdmin:
+                    return "System Admin";
+                case DefaultRole.SuperAdmin:
+                    return "Super Admin";
+                case DefaultRole.Admin:
+                    return "Admin";
+                default:
+                    return "User";
+            }
+        }
+        public enum DefaultUser
+        {
+            SystemAdmin,
+            SuperAdmin
+        }
+    }
+}
