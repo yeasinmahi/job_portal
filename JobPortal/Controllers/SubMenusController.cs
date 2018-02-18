21fd1442a7ab27dc9125a44675f312aef3e69a4a
ManasDatta <manas.aiub.cse@gmail.com>
2018-02-18 17:39:30 +0600

    Modify Menu and MenuItem

--- JobPortal/Controllers/SubMenusController.cs
+++ JobPortal/Controllers/SubMenusController.cs
@@ -10,7 +10,7 @@ namespace JobPortal.Controllers
 {
     public class SubMenusController : BaseController
     {
-        public SubMenusController() : base("Sub Menu")
+        public SubMenusController() : base("SubMenu")
         {
             
         }
